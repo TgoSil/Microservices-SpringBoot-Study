6889820c299fd4b8bb9e4cb4182cb393054dfb78
Tiago Almeida <tiagosilveira0477@gmail.com>
2026-06-06 10:02:17 -0300

    Adicionando arquivos básicos e de configuração

---
+++ patience-service/src/main/java/com/tiago/patience_service/PatienceServiceApplication.java
@@ -0,0 +1,13 @@
+package com.tiago.patience_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatienceServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatienceServiceApplication.class, args);
+	}
+
+}
